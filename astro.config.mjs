39df3f8a35e92fab1bebc2d087e1d2421f5ee211
Adam Sobotka <vorcigernix@gmail.com>
2023-02-11 19:00:13 +0100

    builders

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
+  output: 'server',
+  adapter: netlify({
+    builders: true
+  }),
 });
